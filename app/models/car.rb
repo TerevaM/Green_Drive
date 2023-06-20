4fe55ffc237ec6201a0f0270196332658604b887
Angie Duhard <angie.duhard@gmail.com>
2023-06-21 00:45:57 +0200

    added cloudinary and simpleform for cars

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,6 +2,7 @@ class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
+  has_one_attached :photo
 
   validates :model, presence: true
   validates :user, presence: true
