e20e5cad7e1a1835f18b0623753f8a78aaa9f6af
Adrien Le Doussal <adoussal@gmail.com>
2023-06-20 14:15:09 +0200

    bookings ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :cars
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'cars#index'
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  resources :cars do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :bookings, only: [:index, :show, :destroy]
 end
