aa326be7494519bc444e92ccc2e92c82710c0ef7
Tereva Millet <terevamillet@yahoo.fr>
2023-06-21 19:00:49 +0200

    seed test 1 user + car

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,17 +23,22 @@ rand_mail = ['gmail', 'yahoo', 'free']
                       password: 'motdepasse')
   new_user.save!
   rand(2..5).times do
-
+    start_date = rand(0..31)
+    end_date = start_date + rand(0..60)
     make_and_model = Faker::Vehicle.make_and_model.split
     color_name = Faker::Color.color_name
+    car_pic = Faker::LoremFlickr.image(size: "1920x1080",
+                                       search_terms: ['cars', make_and_model[0], make_and_model[1], color_name])
     new_car = Car.new(model: make_and_model[0],
                       brand: make_and_model[1],
                       color: color_name,
                       year: Faker::Vehicle.year,
                       number_of_seats: rand(2..9),
                       description: Faker::Lorem.paragraphs(number: rand(2..4)),
-                      
-                    )
+                      start_date: Date.now + start_date,
+                      end_date: Date.now + end_date,
+                      rate: rand(45..130),
+                      photo: Cloudinary::Uploader.upload(car_pic))
     new_car.save!
   end
 end
