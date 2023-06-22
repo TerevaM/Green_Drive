a83092f64e480b3669f1a9e61475472a2ac18c3f
Angie Duhard <angie.duhard@gmail.com>
2023-06-22 13:14:52 +0200

    cloudinary add photos

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,10 +2,9 @@ class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
-  has_one_attached :photo
+  has_many_attached :photos
 
   validates :model, presence: true
   validates :brand, presence: true
   validates :description, presence: true
-  validates :photo, presence: true
 end
